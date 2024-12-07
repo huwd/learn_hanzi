63419000b34761c0c7f6a2cbe329610667c6e85a
Huw Diprose <mail@huwdiprose.co.uk>
2024-12-07 21:38:39 +0000

    refactor(Meaning): Rewrite tests should shoulda

--- spec/models/meaning_spec.rb
+++ spec/models/meaning_spec.rb
@@ -1,28 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Meaning, type: :model do
+  describe "associations" do
+    it { should belong_to(:dictionary_entry) }
+  end
+
   describe "validations" do
-    it "is valid with valid attributes" do
-      meaning = build(:meaning)
-      expect(meaning).to be_valid
-    end
-
-    it "is invalid without a dictionary entry" do
-      meaning = build(:meaning, dictionary_entry: nil)
-      expect(meaning).to_not be_valid
-      expect(meaning.errors[:dictionary_entry]).to include("must exist")
-    end
-
-    it "is invalid without a language" do
-      meaning = build(:meaning, language: nil)
-      expect(meaning).to_not be_valid
-      expect(meaning.errors[:language]).to include("can't be blank")
-    end
-
-    it "is invalid without text" do
-      meaning = build(:meaning, text: nil)
-      expect(meaning).to_not be_valid
-      expect(meaning.errors[:text]).to include("can't be blank")
-    end
+    it { should validate_presence_of(:language) }
+    it { should validate_presence_of(:text) }
   end
 end
