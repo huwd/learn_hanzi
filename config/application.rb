9c717f0de0389c64952923730f376894ad2e0dfd
Huw Diprose <mail@huwdiprose.co.uk>
2024-12-07 17:39:50 +0000

    feat(setup): Add and configure FactoryBot

--- config/application.rb
+++ config/application.rb
@@ -30,10 +30,12 @@ module LearnHanzi
 
     config.generators do |g|
       g.test_framework :rspec,
+                      fixtures: true,
                       view_specs: false,
                       helper_specs: false,
                       routing_specs: false,
                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
 
     # Configuration for the application, engines, and railties goes here.
