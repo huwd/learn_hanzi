901adfc481771327f981d5adedee67d01ca3ce3a
Huw Diprose <mail@huwdiprose.co.uk>
2024-12-07 21:37:05 +0000

    refactor(Tags): Shoulda tests

--- spec/factories/tags.rb
+++ spec/factories/tags.rb
@@ -1,6 +1,12 @@
 FactoryBot.define do
   factory :tag do
-    name { "Tag Name" }
-    category { "Category" }
+    name { "HSK 4" }
+    category { "HSK" }
+
+    factory :parent_tag do
+      after(:create) do |tag|
+        create_list(:tag, parent: tag, name: "Chapter 1")
+      end
+    end
   end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,26 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  let(:tag) { build(:tag) }
-  let(:parent_tag) { create(:tag, name: "Super Group") }
-  let(:child_tag) { create(:tag, name: "Sub Group", parent: parent_tag) }
-
-
-  describe "validations" do
-    it "is valid with valid attributes" do
-      expect(tag).to be_valid
-    end
-
-    it "is not valid without a name" do
-      tag.name = nil
-      expect(tag).to_not be_valid
-    end
+  describe "associations" do
+    it { should have_many(:dictionary_entry_tags).dependent(:destroy) }
+    it { should have_many(:dictionary_entries).through(:dictionary_entry_tags) }
+    it { should belong_to(:parent).class_name("Tag").optional }
+    it { should have_many(:children).class_name("Tag").with_foreign_key("parent_id").dependent(:destroy) }
   end
 
-  describe "associations" do
-    it "can have children" do
-      expect(child_tag.parent).to eq(parent_tag)
-      expect(parent_tag.children).to include(child_tag)
-    end
+  describe "validations" do
+    it { should validate_presence_of(:name) }
   end
 end
