0b2a70737233001447602686b163865ec04c0e9c
Huw Diprose <mail@huwdiprose.co.uk>
2024-12-07 21:57:53 +0000

    feat(Tag): Extend Tag Hierarchy tests

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -11,4 +11,35 @@ RSpec.describe Tag, type: :model do
   describe "validations" do
     it { should validate_presence_of(:name) }
   end
+
+  describe "factory" do
+    it "is valid" do
+      expect(build(:tag)).to be_valid
+    end
+  end
+
+  describe "Tag Hierarchy" do
+    describe "Top Level Tags" do
+      it "has no parent" do
+        expect(build(:tag).parent).to be_nil
+      end
+
+      it "may not have any children" do
+        expect(build(:tag).parent).to be_nil
+      end
+    end
+
+    describe "Child Level Tags" do
+      let(:tag) { create(:tag) }
+      let(:child_tag) { create(:tag, parent: tag) }
+
+      it "has a parent" do
+        expect(child_tag.parent).to be(tag)
+      end
+
+      it "is accessible among the parent's children" do
+        expect(tag.children).to include(child_tag)
+      end
+    end
+  end
 end
