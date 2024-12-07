c4bc15fddcce4ab8629513e69adfbef02ffe4e4d
Huw Diprose <mail@huwdiprose.co.uk>
2024-12-07 18:17:08 +0000

    feat(Tags): Add self referental association
    It will allow us to do a basic nesting of tags.
    So we can have a tag that is a child of another tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,6 @@
 class Tag < ApplicationRecord
+  belongs_to :parent, class_name: "Tag", optional: true
+  has_many :children, class_name: "Tag", foreign_key: "parent_id", dependent: :destroy
+
   validates :name, presence: true
 end
