ca6226d94b8c4b2ec906654fbbde5a3c9c0ef0ea
Huw Diprose <mail@huwdiprose.co.uk>
2024-12-08 22:18:44 +0000

    feat(ImportDictionary): Add Helper to find or create source

---
+++ app/helpers/dictionary_import_helper.rb
@@ -0,0 +1,7 @@
+module DictionaryImportHelper
+  def find_or_create_cc_cedict_source(source_name, source_url)
+    Source.find_or_create_by(name: source_name, url: source_url).tap do |source|
+      source.update(date_accessed: Date.today) if source.date_accessed != Date.today
+    end.save!
+  end
+end
