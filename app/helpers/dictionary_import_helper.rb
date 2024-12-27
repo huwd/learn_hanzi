5bf67d99581903cc6c39c7ff57c5a5f8e9201c36
Huw Diprose <mail@huwdiprose.co.uk>
2024-12-27 17:16:59 +0000

    fix(find_or_create_cc_cedict_source)

--- app/helpers/dictionary_import_helper.rb
+++ app/helpers/dictionary_import_helper.rb
@@ -1,7 +1,9 @@
 module DictionaryImportHelper
   def find_or_create_cc_cedict_source(source_name, source_url)
-    Source.find_or_create_by(name: source_name, url: source_url).tap do |source|
+    source = Source.find_or_create_by(name: source_name, url: source_url).tap do |source|
       source.update(date_accessed: Date.today) if source.date_accessed != Date.today
-    end.save!
+    end
+    source.save!
+    source
   end
 end
--- spec/helpers/dictionary_import_helper_spec.rb
+++ spec/helpers/dictionary_import_helper_spec.rb
@@ -2,28 +2,40 @@ require 'rails_helper'
 
 include DictionaryImportHelper
 
-RSpec.describe "find_or_create_cc_cedict_source" do
-  it "creates a new source if it doesn't exist" do
-    expect {
-      find_or_create_cc_cedict_source("CC-CEDICT", "https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.zip")
-    }.to change(Source, :count).by(1)
 
-    source = Source.find_by(name: "CC-CEDICT")
-    expect(source).to be_present
-    expect(source.url).to eq("https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.zip")
-    expect(source.date_accessed).to eq(Date.today)
-  end
-
-  it "updates the date_accessed for an existing source" do
-    existing_source = Source.create!(
+describe "DictionaryImportHelper" do
+  let(:sample_string) { "一口氣 一口气 [yi1 kou3 qi4] /one breath/in one breath/at a stretch/" }
+  let(:sample_source) do
+    {
       name: "CC-CEDICT",
-      url: "https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.zip",
-      date_accessed: Date.yesterday
-    )
-
-    find_or_create_cc_cedict_source("CC-CEDICT", "https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.zip")
-    existing_source.reload
-
-    expect(existing_source.date_accessed).to eq(Date.today)
+      url: "https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.zip"
+    }
   end
+
+  describe "find_or_create_cc_cedict_source" do
+    it "creates a new source if it doesn't exist" do
+      expect {
+        find_or_create_cc_cedict_source("CC-CEDICT", "https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.zip")
+      }.to change(Source, :count).by(1)
+
+      source = Source.find_by(name: "CC-CEDICT")
+      expect(source).to be_present
+      expect(source.url).to eq("https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.zip")
+      expect(source.date_accessed).to eq(Date.today)
+    end
+
+    it "updates the date_accessed for an existing source" do
+      existing_source = Source.create!(
+        name: "CC-CEDICT",
+        url: "https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.zip",
+        date_accessed: Date.yesterday
+      )
+
+      find_or_create_cc_cedict_source("CC-CEDICT", "https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.zip")
+      existing_source.reload
+
+      expect(existing_source.date_accessed).to eq(Date.today)
+    end
+  end
+
 end
