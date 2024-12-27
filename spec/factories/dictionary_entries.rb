3ecfdb6c27587b35fdbb739381b59c03b70a7a46
Huw Diprose <mail@huwdiprose.co.uk>
2024-12-27 20:42:30 +0000

    change(pinyin-location): Update Factories

--- spec/factories/dictionary_entries.rb
+++ spec/factories/dictionary_entries.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :dictionary_entry do
     text { "感动" }
-    pinyin { "gǎn dòng" }
 
     after(:build) do |entry|
       entry.meanings << build(:meaning, dictionary_entry: entry)
