1cef2049c2bf56ffa17439fdf00c68dc480d5be2
Huw Diprose <mail@huwdiprose.co.uk>
2024-12-27 17:19:47 +0000

    add(Meaning): Accept Nested attributes
    This was holding me back from doing a proper cc_cedict import.
    I need to simultaniously create new vocab, meaning and sources

--- app/models/meaning.rb
+++ app/models/meaning.rb
@@ -2,6 +2,8 @@ class Meaning < ApplicationRecord
   belongs_to :dictionary_entry
   belongs_to :source
 
+  accepts_nested_attributes_for :source
+
   validates :language, presence: true
   validates :text, presence: true
 end
