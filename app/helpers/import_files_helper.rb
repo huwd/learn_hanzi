c5c479e7b6bb67bc81b6eb1658be6e8609f9cc62
Huw Diprose <mail@huwdiprose.co.uk>
2024-12-27 21:56:46 +0000

    add(ImportFilesHelper): Move import methods off to a helper

---
+++ app/helpers/import_files_helper.rb
@@ -0,0 +1,31 @@
+require "open-uri"
+require "zip"
+
+module ImportFilesHelper
+  def download_file_to_tmp(url, destination)
+    File.open(destination, "wb") do |file|
+      file.write(URI.open(url).read)
+    end
+  end
+
+  def unzip_file(temp_zip_path, unzip_dir)
+    FileUtils.mkdir_p(unzip_dir)
+    puts "Unzipping files to #{unzip_dir}..."
+
+    Zip::File.open(temp_zip_path) do |zip_file|
+      zip_file.each do |entry|
+        destination_path = File.join(unzip_dir, entry.name)
+        puts "Extracting #{entry.name} to #{destination_path}..."
+        entry.extract(destination_path) { true }
+      end
+    end
+  end
+
+  def confirm_file_presence(file_name_search_string, unzip_dir)
+    puts "Looking for files matching #{file_name_search_string} in #{unzip_dir}..."
+    matched_files = Dir[unzip_dir.join(file_name_search_string)]
+    puts "Found files: #{matched_files.join(', ')}"
+
+    raise "Something is wrong: file not found in tmp!" if matched_files.empty?
+  end
+end
