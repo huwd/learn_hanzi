fc0e86976f4d0c5e58061f64bbd8e326539f8e39
Huw Diprose <mail@huwdiprose.co.uk>
2024-12-27 21:58:49 +0000

    fix(schema): Regenerate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_27_184441) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_27_213410) do
   create_table "dictionary_entries", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", null: false
