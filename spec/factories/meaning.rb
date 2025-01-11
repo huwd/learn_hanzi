b072d7bf1c8e5e362af3f0575cb4432792fe66c8
Huw Diprose <mail@huwdiprose.co.uk>
2025-01-11 22:08:12 +0000

    TOFIXUP meaning factory

--- spec/factories/meaning.rb
+++ spec/factories/meaning.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :meaning do
     association :dictionary_entry
+    association :source
     pinyin { "gǎn dòng" }
     language { "en" }
     text { "to be touched" }
