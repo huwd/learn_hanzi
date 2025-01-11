6a54ef6c487d7b04fa3a97c748eeeff931e6d4a4
Huw Diprose <mail@huwdiprose.co.uk>
2025-01-11 14:25:56 +0000

    feat(Anki): Support checking a deck for compatibility against dictionary entries

--- app/models/anki.rb
+++ app/models/anki.rb
@@ -1,4 +1,12 @@
 module Anki
+  # Going to hard code the ANKI target for now
+  # Mostly as we're dealing with a single read only DB
+  # I know what's in it
+  # However this is something we'll want to vary, to allow
+  # A user to upload and select their own ANKI table and match
+  # with dictionary entries
+  ANKI_DESK_TARGET = "Mandarin: Vocabulary::a. HSK"
+
   class DB < ReadOnlyRecord
     self.abstract_class = true
 
@@ -8,6 +16,17 @@ module Anki
   class Note < DB
     self.table_name = "notes"
     self.primary_key = "id"
+
+    def card_data
+      if anki_deck.present?
+        field_names = anki_deck["flds"].map { |f| f["name"] }
+        Hash[field_names.zip(flds.split("\u001F"))]
+      end
+    end
+
+    def anki_deck
+      @anki_deck ||= Anki.deck
+    end
   end
 
   class Card < DB
@@ -20,5 +39,11 @@ module Anki
     self.table_name = "revlog"
     self.primary_key = "id"
     self.inheritance_column = :_type_disabled
+  end
+
+  def self.deck
+    models_json = Anki::DB.connection.execute("SELECT models FROM col").first["models"]
+    models = JSON.parse(models_json)
+    models.find { |_, model| model["name"] == "HSK" }[1]
   end
 end
