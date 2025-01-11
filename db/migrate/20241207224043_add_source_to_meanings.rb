a1e26a4e27a165f61d7e2ca790f869866c2afb13
Huw Diprose <mail@huwdiprose.co.uk>
2025-01-11 22:20:36 +0000

    Fix schema migrations
    I'd let this get into a bit of a mess, fixing these up now

--- db/migrate/20241207224043_add_source_to_meanings.rb
+++ db/migrate/20241207224043_add_source_to_meanings.rb
@@ -1,6 +1,5 @@
 class AddSourceToMeanings < ActiveRecord::Migration[8.0]
   def change
     add_reference :meanings, :source, foreign_key: true
-    remove_index :meanings, columns: [ :dictionary_entry_id, :language, :text ]
   end
 end
