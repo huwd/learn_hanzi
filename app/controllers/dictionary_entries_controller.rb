f36445fa509e9ef62f62dfa35a5a37022974a1c4
Huw Diprose <mail@huwdiprose.co.uk>
2025-01-12 10:45:01 +0000

    Bring revlog into dictionary entries
    This is awfully slow but works, let's move on to optimise

--- app/controllers/dictionary_entries_controller.rb
+++ app/controllers/dictionary_entries_controller.rb
@@ -2,5 +2,7 @@ class DictionaryEntriesController < ApplicationController
   def show
     @dictionary_entry = DictionaryEntry.find_by_id(params[:id])
     @meanings = @dictionary_entry.meanings.select { |meaning| meaning.language == "en" }
+    @user_learning = UserLearning.find { |ul| ul.user == Current.user && ul.dictionary_entry == @dictionary_entry }
+    @reviews = @user_learning.review_logs
   end
 end
