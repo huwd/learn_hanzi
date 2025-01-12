ee58d37609a21525f99a53d15d0dc1cdb6c1a95d
Huw Diprose <mail@huwdiprose.co.uk>
2025-01-12 10:23:16 +0000

    add UserLearnings can be accessed from a User
    This is quite slow, we'll need to optimise here

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_many :user_learnings, dependent: :destroy
 
   validates :email_address, presence: true
   validates :email_address, uniqueness: true
