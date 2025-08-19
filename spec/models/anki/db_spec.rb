8a5f85eb57a12879c6a42a3dc7513190ca8cf016
Huw Diprose <mail@huwdiprose.co.uk>
2025-08-20 00:26:19 +0100

    Fix db_spec test case
    sqlite_sequence is a reserved table and we'll be removing it shortly

--- spec/models/anki/db_spec.rb
+++ spec/models/anki/db_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
-# Go manually hack the DB to add:
-# INSERT INTO cards (nid, ord, mod, usn, did, queue, due, ivl, reps, lapses, left, factor, type, odue, odid, flags, data) VALUES (1, 1, 1, 1, 1, 1, 1622505600, 10, 4, 1, 1, 10, 0, 1, 1, 1, "")
-# INSERT INTO notes (guid, mid, mod, tags, sfld, usn, csum, flags, flds, data) VALUES ("abc", 1, 1, 1, 1, 1, 1, 1, '20\u001F很\u001F很\u001Fhěn\u001Fhen3\u001Fvery; quite\u001Fadverb\u001F[sound:hen3.mp3]\u001F\u001F\u001F你做得<b>很</b>好。\u001F你做得<b>很</b>好。\u001F你做得[ ]好。\u001F你 得[ ]好。\u001FNǐ zuò de hěn hǎo.\u001FNi3 zuo4 de hen3 hao3.\u001FYou've done great.\u001F[sound:tmpzbp6hb.mp3]\u001F<img src=\"4839396.jpg\" />', '' )
-# INSERT INTO revlog (cid, usn, lastIvl, ease, ivl, time, factor, type) VALUES (1, 1, 1, 2, 10, 300, 2500, 1)
-
 RSpec.describe Anki::DB, type: :model do
   describe ".connection" do
     it "returns a valid SQLite3::Database connection" do
@@ -16,7 +11,6 @@ RSpec.describe Anki::DB, type: :model do
       expect(result.flatten).to include(
        { "name"=>"android_metadata" },
        { "name"=>"cards" },
-       { "name"=>"sqlite_sequence" },
        { "name"=>"col" },
        { "name"=>"graves" },
        { "name"=>"notes" },
